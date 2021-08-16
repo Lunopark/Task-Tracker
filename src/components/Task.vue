2b35d1762d8101c86852c8d6e26c08d2f64fbbbd
Lunopark <rostypod@gmail.com>
2021-08-16 19:34:49 +0300

    task-tracker

---
+++ src/components/Task.vue
@@ -0,0 +1,37 @@
+<template>
+    <div @dblclick="$emit('toggle-reminder')" :class="[task.reminder ? 'reminder' : '', 'task']">
+        <h3>{{task.text}}
+            <i @click="$emit('delete-task', task.id)" class="fas fa-times"></i>
+        </h3>
+        <p>{{task.day}}</p>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Task',
+    props: {
+        task: Object
+    },
+
+}
+</script>
+
+<style scope>
+    .fas{
+        cursor: pointer;
+    }
+    .task{
+        background: #f4f4f4;
+        margin: 5px;
+        padding: 10px 20px;
+    }
+    .task.reminder{
+        border-left: 5px solid black;
+    }
+    .task h3{
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+    }
+</style>
